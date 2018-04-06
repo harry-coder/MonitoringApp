e2405cff9b1f79d5119a15af832ab050298608a4
harry-coder <harpreetsinghvaio14@gmail.com>
2018-04-06 14:08:14 +0530

    Initial commit

---
+++ app/src/main/java/firebasecloud/com/firebasecloud/CustomClasses/FormattedAddressPojo.java
@@ -0,0 +1,8 @@
+package firebasecloud.com.firebasecloud.CustomClasses;
+
+/**
+ * Created by Harpreet on 03/04/2018.
+ */
+
+public class FormattedAddressPojo {
+}
---
+++ app/src/main/java/firebasecloud/com/firebasecloud/CustomClasses/RelatedVideoModel.java
@@ -0,0 +1,47 @@
+package firebasecloud.com.firebasecloud.CustomElements;
+
+/**
+ * Created by Harpreet on 19/03/2018.
+ */
+
+public class RelatedVideoModel {
+
+    String videoId;
+    String imageUrl;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    String description;
+
+    public String getVideoId() {
+        return videoId;
+    }
+
+    public void setVideoId(String videoId) {
+        this.videoId = videoId;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    String title;
+}
